569cbec7f63a4bf90017f8716fbcfda8ae403c61
Denzel Sullivan <Denzel.W.Sullivan@gmail.com>
2023-01-16 22:24:35 -0500

    Commiting Java exercise 1

---
+++ src/JavaExercise1.java
@@ -0,0 +1,51 @@
+public class JavaExercise1 {
+
+    public static void main(String[] args) {
+        System.out. println();
+        System.out.println("Int Example");
+        intEx();
+        System.out. println();
+        System.out.println("String Example");
+        stringEx();
+        System.out. println();
+        System.out.println("Loop Example");
+        loopEx();
+    }
+
+    public static void intEx(){
+        // Create an int variable and assign a value
+
+        // Do some math and add a value to your int and print it out
+
+        // Create an if statement and print if the value is greater than 10
+
+        // Create a when statement and print if the value is positive, negative or zero
+
+        // Operators:
+        // Increment your number and print it out
+        // Decrement your number and print it out
+        // Use the modulus operator to check if your number is odd or even and print out the result
+    }
+
+    public static void stringEx(){
+        // Create 2 string variables firstName and lastName and initialize them
+
+        // Concatenate the 2 strings into one string called fullName and print it out
+
+        // Find the length of the fullName string and print it out
+
+        // Print out the first initial of the firstName and entire lastName
+
+        // Compare the lengths of the first and last name and print out which is longer
+    }
+
+    public static void loopEx(){
+        // Create a while loop and print out 1-5
+
+        // Create a for loop and print out 5-1
+
+        // Create a loop and only print out even numbers (if/else first then with continue)
+
+        // Create a loop from 1-10. print out when the loop is on 5 and stop the loop
+    }
+}
